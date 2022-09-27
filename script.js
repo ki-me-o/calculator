2c4b10309ca76bac16781953e5787338c0978040
Sebastian Subola <sebastian.subola@gmail.com>
2022-09-27 09:44:49 -0600

    Add listeners to number buttons to update display

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 /* Global variables */
-let cacheValue,
-    displayValue,
+let cacheFloat,
+    displayString = '',
     activeOperator;
 
 
 /* DOM Manipulation */
 initializeNineKey();
-
+initalizeNumberButtonsListeners();
 
 /* Function declarations */
 
@@ -29,6 +29,7 @@ function calcDivide(a,b) {
 }
 
 function operate(a,b,operator) {
+    // This would be a convenient place to round to so many decimal places
     return (operator(a,b));
 }
 
@@ -41,4 +42,17 @@ function initializeNineKey() {
         numKey.textContent = i;
         nineKey.appendChild(numKey);
     }
+}
+
+function initalizeNumberButtonsListeners() {
+    const numKeys = Array.from(document.querySelectorAll('button.number'));
+    numKeys.forEach(num => {
+        num.addEventListener('click', e => {
+            console.log('displayString before: ' + displayString);
+            displayString = displayString.concat('',num.innerText);
+            console.log('displayString after press: ' + displayString);
+        });
+    });
+
+    
 }
