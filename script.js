5f4f31f8b22701d9bdf0e0cc4067b25baf4a9bca
Sebastian Subola <sebastian.subola@gmail.com>
2022-09-26 18:38:55 -0600

    Add basic add, sub, mult, div functions

---
+++ script.js
@@ -0,0 +1,24 @@
+/* Global variables */
+
+
+/* DOM Manipulation */
+
+
+
+/* Function declarations */
+function calcAdd (a,b) {
+    return a+b;
+}
+
+function calcSubtract(a,b) {
+    return a-b;
+}
+
+function calcMultiply(a,b) {
+    return a*b;
+}
+
+function calcDivide(a,b) {
+    if(b === 0) return "ERROR";
+    return a / b;
+}
